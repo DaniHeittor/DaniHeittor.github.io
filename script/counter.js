03f3ef77045b87fe4b270122fa26c67f5fbfa63e
DaniHeittor <59509028+DaniHeittor@users.noreply.github.com>
2020-07-20 11:11:44 -0100

    Update counter.js

--- script/counter.js
+++ script/counter.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jul 20, 2020 0:00:00").getTime();
+var countDownDate = new Date("Jul 22, 2020 0:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
